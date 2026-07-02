2b7e94627c47b1e2ddc5cf2b380822698a777f26
teleosy <76188847+teleosy@users.noreply.github.com>
2026-07-02 09:29:31 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+
+# Import the sentiment_analyzer function from the package created:
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app :
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs sentiment analysis over it using sentiment_analysis()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid input! Try again."
+
+    return (
+        f"For the given statement, the system response is "
+        f"anger': {response['anger']}, "
+        f"disgust': {response['disgust']}, "
+        f"fear': {response['fear']}, "
+        f"joy': {response['joy']}, "
+        f"sadness': {response['sadness']}, "
+        f"The dominant emotion is {response['dominant_emotion]']}."
+    )
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
